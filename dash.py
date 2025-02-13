c3b667b109f07988bf313f9696b0f510f803266b
seamusprs <seamusprs>
2025-02-13 10:53:01 -0800

    start dash app template

---
+++ dash.py
@@ -0,0 +1,44 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import altair as alt
+import pandas as pd
+import os
+
+# read in, import, combine data
+# ---------------------------------
+
+seasons = []
+rows_total = 0
+
+for season in os.listdir("data"):
+    # print(season)
+    if season[-4:] == ".csv":
+        seasons.append(pd.read_csv("data/" + season, on_bad_lines="warn", encoding='windows-1252'))
+
+for season in seasons:
+    rows_total += len(season)
+    season["Date"] = pd.to_datetime(season["Date"], format = "mixed", dayfirst = True)
+
+df = pd.concat(seasons, axis = 0, ignore_index = True).dropna(axis = 1, how = "all").dropna(axis = 0, how = "all")
+
+# print("%.2f %% of original data imported successfully" % (len(df) / rows_total * 100))
+# print("%i rows dropped." % (rows_total - len(df)))
+
+
+# set up app and layout / frontend
+# ---------------------------------
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div("Hello world!")
+
+
+# set up callbacks / backend
+# ---------------------------------
+
+if __name__ == "__main__":
+    app.run_server = True
+
+

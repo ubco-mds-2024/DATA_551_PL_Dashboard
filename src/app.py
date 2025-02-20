d57ffed43a0b71b6c8d6c144585ed23e512f61a6
seamusprs <seamusprs>
2025-02-20 14:45:52 -0800

    add filters to app

--- src/app.py
+++ src/app.py
@@ -15,7 +15,9 @@ rows_total = 0
 for season in os.listdir("../data/raw"):
     # print(season)
     if season[-4:] == ".csv":
-        seasons.append(pd.read_csv("../data/raw/" + season, on_bad_lines="warn", encoding='windows-1252'))
+        thisseason = pd.read_csv("../data/raw/" + season, on_bad_lines="warn", encoding='windows-1252')
+        thisseason["Season"] = str(season[0:2]) + "/" + str(season[2:4])
+        seasons.append(thisseason)
 
 for season in seasons:
     rows_total += len(season)
@@ -26,6 +28,54 @@ df = pd.concat(seasons, axis = 0, ignore_index = True).dropna(axis = 1, how = "a
 print("%.2f %% of original data imported successfully" % (len(df) / rows_total * 100))
 print("%i rows dropped." % (rows_total - len(df)))
 
+team_codes = {
+    "Burnley": "BRN",
+    "Huddersfield": "HUD",
+    "Ipswich": "IPS",
+    "Portsmouth": "POR",
+    "Wigan": "WIG",
+    "Nott'm Forest": "NFO",
+    "Stoke": "STO",
+    "Liverpool": "LIV",
+    "Luton": "LUT",
+    "Man City": "MCI",
+    "Sunderland": "SUN",
+    "Leeds": "LEE",
+    "Swansea": "SWA",
+    "Chelsea": "CHE",
+    "QPR": "QPR",
+    "Bournemouth": "BOU",
+    "Watford": "WAT",
+    "Derby": "DER",
+    "Charlton": "CHA",
+    "Bolton": "BOL",
+    "Reading": "REA",
+    "Brighton": "BHA",
+    "Newcastle": "NEW",
+    "Fulham": "FUL",
+    "West Brom": "WBA",
+    "Middlesborough": "MID",
+    "Norwich": "NOR",
+    "Birmingham": "BIR",
+    "Blackburn": "BBR",
+    "Everton": "EVE",
+    "Tottenham": "TOT",
+    "Hull": "HUL",
+    "Cardiff": "CAR",
+    "Aston Villa": "AVL",
+    "Man United": "MUN",
+    "Crystal Palace": "CRY",
+    "Arsenal": "ARS",
+    "Sheffield United": "SHU",
+    "Southampton": "SOU",
+    "Wolves": "WOL",
+    "West Ham": "WHU",
+    "Leicester": "LEI",
+    "Brentford": "BRE",
+    "Blackpool": "BLA"
+}
+
+stats = ["Goals"]
 
 # set up app and layout / frontend
 # ---------------------------------
@@ -35,13 +85,25 @@ app = dash.Dash(external_stylesheets = [dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container([
     html.H1("Premier League Dashboard"),
     dbc.Row([
-        html.H2("Filters"),
-        dbc.Col([
-            html.H2("Checklist of teams here")
-        ], md = 2),
-                dbc.Col([
-            html.H2("Checklist of seasons here")
-        ], md = 2),
+            dcc.Dropdown(
+                id = "stat-list",
+                value = "Goals",
+                options = [{"label": stat, "value": stat} for stat in stats]
+            ),
+            dbc.Col([
+                dcc.Checklist(
+                    id = "teams-list",
+                    options = [{"label": team, "value": team_codes[team]} for team in sorted(team_codes)],
+                    style = {"overflowY": "scroll", "max-height": "30%", "max-width": "100%"}
+                )
+            ], md = 2),
+                    dbc.Col([
+                dcc.Checklist(
+                    id = "seasons-list",
+                    options = [{"label": season, "value": season} for season in sorted(list(set(df["Season"])))],
+                    style = {"overflowY": "scroll", "max-height": "30%", "max-width": "100%"}
+                )
+            ], md = 2),
         dbc.Col([
             html.H2("Per team chart here")
         ], md = 4),
@@ -49,6 +111,7 @@ app.layout = dbc.Container([
             html.H2("Per season chart here")
         ], md = 4)
     ], className = "h-50"),
+
     dbc.Row([ 
         html.H2("Season by season timeline here")
     ], className = "h-25"),
@@ -61,8 +124,13 @@ app.layout = dbc.Container([
 # set up callbacks / backend
 # ---------------------------------
 
+@app.callback(
+    Output("pie1", "srcDoc"),
+    Input("teams-list", "value"),
+    Input("seasons-list", "value")
+)
+def plot_altair(xcol, ycol):
+    pass
+
 if __name__ == "__main__":
     app.run_server(debug = True)
-    
-
-
